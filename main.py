423a5c42d90f623f9fc7d9d151cfdc976b93d926
sulajmanovseraly-boop <sulajmanovseraly@gmail.com>
2025-10-05 21:23:43 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+import os
+
+TOKEN = os.getenv("8478347349:AAFwYo4-W9k4K19MfRM9gTiITja0VbEXCkE")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Отправь своё предложение или жалобу — я передам президенту школы. Всё анонимно 👀")
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    with open("feedback.txt", "a", encoding="utf-8") as f:
+        f.write(f"{message.text}\n---\n")
+    bot.reply_to(message, "✅ Спасибо! Сообщение отправлено анонимно.")
+
+bot.infinity_polling()
